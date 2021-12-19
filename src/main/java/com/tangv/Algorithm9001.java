73c60057bf93b48bed6f84754072445c1061df9f
tangvy <949129882@qq.com>
2021-12-19 15:56:38 +0800

    求数组中最大值

---
+++ src/main/java/com/tangv/Algorithm9001.java
@@ -0,0 +1,17 @@
+package com.tangv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author:   tangwei
+ * Date:     2021/12/19 9:53
+ * Description:
+ */
+@SpringBootApplication
+public class Algorithm9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Algorithm9001.class, args);
+    }
+
+}
